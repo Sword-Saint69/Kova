73345e0a3b3c1021c8ecb634c508cc6bd5b5df85
GOUTHAM SANKAR J L <gouthamsankar@aol.com>
2026-03-21 00:51:33 +0530

    1.168

--- kova-vue/src/utils/db.js
+++ kova-vue/src/utils/db.js
@@ -1,5 +1,3 @@
 import { neon } from '@neondatabase/serverless';
 
-const sql = neon(import.meta.env.VITE_DATABASE_URL || process.env.DATABASE_URL);
-
-export default sql;
+export const sql = neon(import.meta.env.VITE_DATABASE_URL || process.env.DATABASE_URL);
