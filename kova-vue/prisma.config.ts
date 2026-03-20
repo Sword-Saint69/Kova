eb12eef29e0307ccbc283c3aed438d8c591c09af
GOUTHAM SANKAR J L <gouthamsankar@aol.com>
2026-03-20 23:39:38 +0530

    1.99

--- kova-vue/prisma.config.ts
+++ kova-vue/prisma.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from '@prisma/config';
 export default defineConfig({
   schema: './prisma/schema.prisma',
   datasource: {
-    url: "postgresql://neondb_owner:npg_9VQsHfhvxJS2@ep-polished-rice-ajlklfzx.us-east-2.aws.neon.tech/neondb?sslmode=require",
+    url: process.env.DATABASE_URL,
   },
 });
