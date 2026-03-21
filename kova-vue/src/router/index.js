c4e703448f5f25ad0888f2895b4c568905f31671
GOUTHAM SANKAR J L <gouthamsankar@aol.com>
2026-03-22 04:53:23 +0530

    1.298

--- kova-vue/src/router/index.js
+++ kova-vue/src/router/index.js
@@ -7,6 +7,7 @@ const routes = [
   { path: '/login',     component: () => import('../views/Login.vue')    },
   { path: '/dashboard', component: () => import('../views/Dashboard.vue'), meta: { requiresAuth: true } },
   { path: '/habits/new', component: () => import('../views/NewHabit.vue'), meta: { requiresAuth: true } },
+  { path: '/habits/:id', component: () => import('../views/HabitDetail.vue'), meta: { requiresAuth: true } },
 ];
 
 const router = createRouter({
